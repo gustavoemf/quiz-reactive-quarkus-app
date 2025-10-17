9a7f4029b769db16d25dd00b0c670483c81a9cd6
Gustavo Muller <gustavoeduardomf@gmail.com>
2025-10-17 15:49:11 -0300

    dependencia mapStruct e implementação do mapper full

--- src/main/java/io/gustavoemf/mapping/QuizFullUpdateMapper.java
+++ src/main/java/io/gustavoemf/mapping/QuizFullUpdateMapper.java
@@ -1,4 +1,14 @@
 package io.gustavoemf.mapping;
 
-public class QuizFullUpdateMapper {
+import io.gustavoemf.domain.Quiz;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants.ComponentModel;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = ComponentModel.JAKARTA_CDI)
+public interface QuizFullUpdateMapper {
+
+    @Mapping(target = "id", ignore = true)
+    void mapFullUpdate(Quiz input, @MappingTarget Quiz target);
 }
